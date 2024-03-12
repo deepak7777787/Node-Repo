8beebc7788d870b03ca41d8eebb491bd81d078a0
deepak <deepakdata07@gmail.com>
2024-03-12 22:41:18 +0530

    server-app only added

--- modules.js
+++
@@ -1,7 +0,0 @@
-const { people, ages } = require('./names');
-
-console.log(people, ages);
-
-const os = require('os');
-
-console.log(os.platform(), os.homedir());
--- names.js
+++
@@ -1,7 +0,0 @@
-const people = ['deepak', 'vijay', 'kabilan'];
-const ages = [20,21,22];
-
-console.log(people);
-
-module.exports = {people, ages};
-
