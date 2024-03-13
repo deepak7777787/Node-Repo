b4104cedbe053884b5bed8e300fddb0110d3ab32
deepak <deepakdata07@gmail.com>
2024-03-13 16:04:31 +0530

    7-view-engine branch added

--- server-app/app.js
+++ server-app/app.js
@@ -3,25 +3,30 @@ const express = require('express');
 // express app
 const app = express();
 
+// registen view engine
+app.set('view engine', 'ejs');
+
 // listen for requests
-app.listen(3000);
+app.listen(3000); 
 
 app.get('/', (req, res) => {
-    // res.send('<p>Home page</p>');
-    res.sendFile('./views/index.html', { root: __dirname});
+    const events = [
+        {title: 'mongodb tutorial', snippet: 'super buddy. this is an event scheduling site'},
+        {title: 'mongodb tutorial', snippet: 'super buddy. this is an event scheduling site'},
+        {title: 'mongodb tutorial', snippet: 'super buddy. this is an event scheduling site'},
+    ];
+    res.render('index', { title: 'Home', events });
 });
 
 app.get('/about', (req, res) => {
-    // res.send('<p>About page</p>');
-    res.sendFile('./views/about.html', { root: __dirname});
+    res.render('about', { title: 'About' });
 });
 
-// redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+app.get('/events/create', (req, res) => {
+    res.render('create', { title: 'Create a new Event' });
 });
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname});
+    res.status(404).render('404', { title: '404' });
 });
